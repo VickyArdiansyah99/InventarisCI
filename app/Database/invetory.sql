786cc6120c00af9697891d7d6803422911864679
VickyArdiansyah99 <ardianvicky12345@gmail.com>
2024-12-09 22:35:26 +0700

    perubahan database

--- app/Database/invetory.sql
+++ app/Database/invetory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 30, 2024 at 07:12 AM
+-- Generation Time: Dec 09, 2024 at 03:10 PM
 -- Server version: 8.0.30
 -- PHP Version: 7.4.9
 
@@ -47,7 +47,10 @@ CREATE TABLE `tbl_barang` (
   `id_barang` int NOT NULL,
   `nama_barang` varchar(255) NOT NULL,
   `nama_kategori` varchar(20) NOT NULL,
+  `merk` varchar(20) NOT NULL,
+  `spesifikasi` varchar(255) NOT NULL,
   `tanggal_pembelian` date DEFAULT NULL,
+  `harga` int NOT NULL,
   `kondisi` varchar(50) DEFAULT NULL,
   `id_lokasi` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -56,11 +59,12 @@ CREATE TABLE `tbl_barang` (
 -- Dumping data for table `tbl_barang`
 --
 
-INSERT INTO `tbl_barang` (`id_barang`, `nama_barang`, `nama_kategori`, `tanggal_pembelian`, `kondisi`, `id_lokasi`) VALUES
-(2, 'PC', 'CPU', '2024-08-10', 'baik', 1),
-(4, 'meja', 'BARANG', '2024-08-08', 'baik', 1),
-(5, 'mause', 'elektronik', '2024-08-14', 'baik', 11),
-(6, 'meja', 'BARANG', '2024-08-23', 'rusak', 2);
+INSERT INTO `tbl_barang` (`id_barang`, `nama_barang`, `nama_kategori`, `merk`, `spesifikasi`, `tanggal_pembelian`, `harga`, `kondisi`, `id_lokasi`) VALUES
+(2, 'PC', 'Peralatan Elektronik', 'HP', 'Processor: Intel Core i3 1115G4\r\nRAM: 4GB DDR4, SSD: 512GB\r\nODD: DVDRW, VGA: Integrated\r\nUkuran Layar: 21.5 Inch FHD\r\nSistem Operasi: Windows 10\r\nHP All-in-One 22-df1001d', '2024-08-10', 8500000, 'baik', 1),
+(4, 'meja', 'Perabotan', 'Expo MDC 1075', 'panjang: 100 cm\r\nlebar: 75 cm\r\ntinggi: 75 cm\r\nwarna: coklat', '2024-08-08', 1700000, 'baik', 1),
+(5, 'mouse', 'Peralatan Elektronik', 'HP X1000', 'Tipe: kabel\r\nDPI: 1000 DPI\r\nFitur: Desain sederhana, plug-and-play, kompatibilitas universal dengan USB', '2024-08-14', 100000, 'baik', 11),
+(6, 'kursi', 'Perabotan', 'BW 602 UPC', 'panjang: 74 cm\r\nlebar: 44cm\r\ntinggi 82 cm\r\nbahan: besi\r\nwarna: hitam\r\n', '2024-08-23', 640000, 'rusak', 2),
+(7, 'cctv', 'Peralatan Keamanan', 'Hikvision', 'Model : Kikvision DS-2CD2143G0-I\r\nResolusi : 4MP\r\nLensa: 4mm', '2023-12-08', 1500000, 'baik', 8);
 
 -- --------------------------------------------------------
 
@@ -106,15 +110,16 @@ CREATE TABLE `tbl_user` (
 INSERT INTO `tbl_user` (`id_user`, `nama_user`, `username`, `password`, `level`, `foto`) VALUES
 (1, 'Vicky', 'admin', 'admin', 0, 'foto.jpg'),
 (2, 'Athor', 'user', '123', 2, 'user.png'),
-(4, 'User', 'direktur', '123', 1, 'user.png');
+(4, 'User', 'direktur', '123', 1, 'user.png'),
+(6, 'sasa', 'sasa', '123', 2, 'user.png');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_usulan`
+-- Table structure for table `tbl_usulan`
 --
 
-CREATE TABLE `tb_usulan` (
+CREATE TABLE `tbl_usulan` (
   `id_` int NOT NULL,
   `id_pengusul` varchar(40) NOT NULL,
   `namaBarang` text NOT NULL,
@@ -126,13 +131,12 @@ CREATE TABLE `tb_usulan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tb_usulan`
+-- Dumping data for table `tbl_usulan`
 --
 
-INSERT INTO `tb_usulan` (`id_`, `id_pengusul`, `namaBarang`, `jmlBarang`, `kiraHarga`, `jmlHarga`, `keperluan`, `status`) VALUES
+INSERT INTO `tbl_usulan` (`id_`, `id_pengusul`, `namaBarang`, `jmlBarang`, `kiraHarga`, `jmlHarga`, `keperluan`, `status`) VALUES
 (2, '1', 'kursi', '20', '50000', '1000000', 'kelengkapan kelas', '2'),
 (5, '1', 'komputer', '12', '100000', '1200000', 'kebutuhan lab', '0'),
-(6, '', 'komputer', '20', '7000000', '140000000', 'ssassaaa', '2'),
 (7, '', 'meja', '10', '60000', '600000', 'dsadsa', '2');
 
 --
@@ -165,9 +169,9 @@ ALTER TABLE `tbl_user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- Indexes for table `tb_usulan`
+-- Indexes for table `tbl_usulan`
 --
-ALTER TABLE `tb_usulan`
+ALTER TABLE `tbl_usulan`
   ADD PRIMARY KEY (`id_`);
 
 --
@@ -184,7 +188,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `tbl_barang`
 --
 ALTER TABLE `tbl_barang`
-  MODIFY `id_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tbl_lokasi`
@@ -196,12 +200,12 @@ ALTER TABLE `tbl_lokasi`
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `tb_usulan`
+-- AUTO_INCREMENT for table `tbl_usulan`
 --
-ALTER TABLE `tb_usulan`
+ALTER TABLE `tbl_usulan`
   MODIFY `id_` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
